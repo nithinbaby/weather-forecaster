fc75895f8882bdbdb9b897bd94022c5cfe9144b4
nithinbaby <nithinb24@gmail.com>
2019-01-13 14:42:41 +0530

    http to https

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
 app.constant('AppConstants', {
-  WEATHER_API_BASE_URL: "http://api.openweathermap.org/data/2.5/forecast",
+  WEATHER_API_BASE_URL: "https://api.openweathermap.org/data/2.5/forecast",
   OPEN_WEATHER_MAP_API_KEY: "57abb797a51e721c8c09c0d0dde639f6"
 });
